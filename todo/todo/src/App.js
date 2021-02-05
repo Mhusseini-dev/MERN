8ce14dbc6f1fecb1017e23efd99a8a13ca95bbd6
Mohamad Husseini <mohusseini@outlook.com>
2021-02-05 21:08:41 +0200

    to do list

---
+++ todo/todo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import TodoComponent from './components/TodoComponent';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <TodoComponent/>
+    </div>
+  );
+}
+
+export default App;
